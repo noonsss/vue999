337b3719469c52e59f88955307a32502eef587c6
noonsss <87642296+noonsss@users.noreply.github.com>
2021-11-18 10:24:51 +0900

    11.18

--- src/views/Reference.vue
+++ src/views/Reference.vue
@@ -11,67 +11,67 @@
             <h3>HTML</h3>
             <ul>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">1</span>
                   <span className="attr">&lt;a&gt;</span>
                   <span className="desc"
                     >&lt;a&gt; 태그는 다른 페이지 이동을 설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">2</span>
                   <span className="attr">&lt;abbr&gt;</span>
                   <span className="desc"
                     >&lt;abbr&gt; 태그는 다른 페이지 이동을 설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">3</span>
                   <span className="attr">&lt;acronym&gt;</span>
                   <span className="desc"
                     >&lt;acronym&gt; 태그는 다른 페이지 이동을 설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">4</span>
                   <span className="attr">&lt;address&gt;</span>
                   <span className="desc"
                     >&lt;address&gt; 태그는 다른 페이지 이동을 설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">5</span>
                   <span className="attr">&lt;applet&gt;</span>
                   <span className="desc"
                     >&lt;applet&gt; 태그는 다른 페이지 이동을 설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">6</span>
                   <span className="attr">&lt;area&gt;</span>
                   <span className="desc"
                     >&lt;area&gt; 태그는 이미지 맵의 영역을 정의합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">7</span>
                   <span className="attr">&lt;article&gt;</span>
                   <span className="desc"
@@ -79,10 +79,10 @@
                     설정합니다.
                   </span>
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">8</span>
                   <span className="attr">&lt;aside&gt;</span>
                   <span className="desc"
@@ -90,30 +90,30 @@
                     설정합니다</span
                   >
                   <span className="Inline">block Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">9</span>
                   <span className="attr">&lt;audio&gt;</span>
                   <span className="desc"
                     >&lt;audio&gt; 태그는 오디오 파일을 설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">10</span>
                   <span className="attr">&lt;b&gt;</span>
                   <span className="desc"
                     >&lt;b&gt; 태그는 다른 텍스트와 구별 할 때 설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">11</span>
                   <span className="attr">&lt;base&gt;</span>
                   <span className="desc"
@@ -121,10 +121,10 @@
                     설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">12</span>
                   <span className="attr">&lt;basefont&gt;</span>
                   <span className="desc"
@@ -132,20 +132,20 @@
                     설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">13</span>
                   <span className="attr">&lt;a&gt;</span>
                   <span className="desc"
                     >&lt;a&gt; 태그는 다른 페이지 이동을 설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">14</span>
                   <span className="attr">&lt;bdi&gt;</span>
                   <span className="desc"
@@ -153,30 +153,30 @@
                     설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">15</span>
                   <span className="attr">&lt;bdo&gt;</span>
                   <span className="desc"
                     >&lt;bdo&gt; 태그는 텍스트의 방향을 설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">16</span>
                   <span className="attr">&lt;big&gt;</span>
                   <span className="desc"
                     >&lt;big&gt; 태그는 텍스트의 크기를 크게 설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">17</span>
                   <span className="attr">&lt;blockquote&gt;</span>
                   <span className="desc"
@@ -184,20 +184,20 @@
                     설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">18</span>
                   <span className="attr">&lt;blink&gt;</span>
                   <span className="desc"
                     >&lt;blink&gt; 태그는 다른 페이지 이동을 설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
               <li>
-                <a href="referDetail.html">
+                <router-link href="/referDetail">
                   <span className="num">19</span>
                   <span className="attr">&lt;body&gt;</span>
                   <span className="desc"
@@ -205,7 +205,7 @@
                     정보 등을 설정합니다.</span
                   >
                   <span className="Inline">Inline Element</span>
-                </a>
+                </router-link>
               </li>
             </ul>
           </div>
@@ -274,7 +274,6 @@ export default {
 
       span {
         display: inline-block;
-        float: left;
         padding: 25px 10px;
         font-size: 14px;
         font-family: "S-CoreDream-3Light";
